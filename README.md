7cb53370a198d5a21a6e64a72052435359d63aef
Razak Addo <addorazak8@gmail.com>
2019-10-14 19:37:14 -0800

    Added get hall members route

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-# CSCD311-class-project-api-10677768
+# CSCD311-class-project-api
+
+Abdul-Razak Addo Musah (10677768)
 Backend
 
 # How to setup
+
 1.  Clone the repo
 2.  Open the project folder in a terminal, and run npm install.
 3.  Run node index.js
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const studentSchema = new mongoose.Schema({
   student_id: Number,
   pin: Number,
@@ -19,15 +19,14 @@ const studentSchema = new mongoose.Schema({
 const Students = mongoose.model("Student", studentSchema, "students");
 app.post("/login", async (req, res) => {
   let student = await Students.findOne(req.body);
-  if (student)
-    res.json(student);
-  else
-    res.send("not_found");
-})
+  if (student) res.json(student);
+  else res.send("not_found");
+});
 
 app.post("/register", async (req, res) => {
   let student = await Students.findOne({ student_id: req.body.student_id });
-  if (student.hall) // Student already registered
+  if (student.hall)
+    // Student already registered
     res.send("fail");
   else {
     student.hall = req.body.hall;
@@ -35,9 +34,16 @@ app.post("/register", async (req, res) => {
     await student.save();
     res.send("success");
   }
-})
+});
 
-mongoose.connect("mongodb+srv://joe:mongojoe18@cluster0-7mtcg.mongodb.net/ug-hall?retryWrites=true&w=majority");
+app.get("/hall_members", async (req, res) => {
+  let students = await Students.find({ hall: req.query.hall });
+  res.send(students);
+});
+
+mongoose.connect(
+  "mongodb+srv://joe:mongojoe18@cluster0-7mtcg.mongodb.net/ug-hall?retryWrites=true&w=majority"
+);
 app.listen(1000, () => {
   console.log("Web Server Started");
-})
+});
